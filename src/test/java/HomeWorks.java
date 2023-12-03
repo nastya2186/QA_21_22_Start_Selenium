0fa3c1aa5dee4480bbaf6b5eb36ce6297be0fb5a
nastya2186 <nastyakudriashev@gmail.com>
2023-12-03 12:59:48 +0200

    added webelements

--- src/test/java/HomeWorks.java
+++ src/test/java/HomeWorks.java
@@ -1,6 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class HomeWorks {
 
@@ -9,6 +14,43 @@ public class HomeWorks {
     @BeforeClass
     public void setUp(){
         wd = new ChromeDriver();
+        wd.get("https://telranedu.web.app/login");
+
+    }
+
+    @Test
+    public void cssLocators(){
+
+        //by tag name
+
+        WebElement e1 = wd.findElement(By.tagName("body"));
+        List<WebElement> list1 = wd.findElements(By.tagName("div"));
+        WebElement e2 = wd.findElement(By.cssSelector("h1"));
+        WebElement e3 = wd.findElement(By.tagName("form"));
+        List<WebElement> list2 = wd.findElements(By.tagName("button"));
+        WebElement e4 = wd.findElement(By.tagName("br"));
+
+        //by class
+        WebElement divcon = wd.findElement(By.className("container"));
+        WebElement divcon1 = wd.findElement(By.className("navbar-component_nav__1X_4m"));
+        WebElement divcon2 = wd.findElement(By.cssSelector(".active"));
+        WebElement divcon3 = wd.findElement(By.className("login_login__3EHKB"));
+
+
+        //by id
+        WebElement id1 = wd.findElement(By.id("root"));
+        WebElement id2 = wd.findElement(By.cssSelector("#root"));
+
+
+        //by attribute
+        WebElement at1 = wd.findElement(By.cssSelector("[href = '/home']"));
+        WebElement at2 = wd.findElement(By.cssSelector("[href = '/about']"));
+        WebElement at3 = wd.findElement(By.cssSelector("[href = '/login']"));
+        WebElement at4 = wd.findElement(By.cssSelector("[placeholder = 'Email']"));
+        WebElement at5 = wd.findElement(By.cssSelector("[placeholder = 'Password']"));
+
+
+
 
     }
 
